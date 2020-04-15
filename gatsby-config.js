c0a21dc4f959548f3daabede82da9c036c2d97aa
Stuart Myers <stuart.myers@gmail.com>
2020-04-15 19:15:09 +0800

    remove image plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,15 +7,15 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `images`,
+    //     path: `${__dirname}/src/images`,
+    //   },
+    // },
+    // `gatsby-transformer-sharp`,
+    // `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
