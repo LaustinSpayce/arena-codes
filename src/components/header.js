07cb9c7d9160bebeae1cd44d5de96c466d3a3bd2
Stuart Myers <stuart.myers@gmail.com>
2020-04-15 20:54:59 +0800

    improve header

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,49 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import Container from "react-bootstrap/Container"
+import { FaGithub } from "react-icons/fa"
 
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#343a40`,
       marginBottom: `1.45rem`,
     }}
   >
-    <div
+    <Container
       style={{
         margin: `0 auto`,
-        maxWidth: 960,
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+      {" "}
+      <div className="row">
+        <h1 style={{ margin: 0 }} className="col-11">
+          <Link
+            to="/"
+            style={{
+              color: `white`,
+              textDecoration: `none`,
+            }}
+          >
+            {siteTitle}
+          </Link>
+        </h1>
+        <h1 style={{ margin: 0 }} className="col-1">
+          <a
+            href="https://github.com/LaustinSpayce/arena-codes"
+            style={{
+              color: `white`,
+              textDecoration: `none`,
+              alignContent: `right`,
+            }}
+          >
+            <FaGithub />
+          </a>
+        </h1>
+      </div>
+    </Container>
   </header>
 )
 
