aac9eed64f7d4d8e7861aa3a7e08a6c5463e9994
Stuart Myers <stuart.myers@gmail.com>
2020-04-15 16:33:28 +0800

    Loops through and returns card names

--- src/components/carddescription.js
+++ src/components/carddescription.js
@@ -1,24 +1,40 @@
 import React, { useState, useRef, useEffect } from "react"
 
-const SCRYFALL_SEARCH = 'https://scryfall.com/search?q='
-const CARDRE = /\[\[([^\]]+)\]\]/g;
+const SCRYFALL_SEARCH = "https://scryfall.com/search?q="
+const CARDRE = /\[\[([^\]]+)\]\]/g
 
 const CardLink = (cardName) => {
-
   const fullURL = SCRYFALL_SEARCH + cardName.name
-  return(<a href={fullURL} target="_blank">{cardName.name}</a>)
+  return (
+    <a href={fullURL} target="_blank">
+      {cardName.name}
+    </a>
+  )
 }
 
-
 const CardDescription = (data) => {
-  const [description, setDescription] = useState(data.description)
+  const cardsArray = data.description.match(CARDRE)
+  let descriptionString = data.description
+  let description = data.description
+  if (cardsArray) {
+    description = cardsArray.map((card) => {
+      let cardName = card.replace("[[", "")
+      cardName = cardName.replace("]]", "")
+      let index = descriptionString.search(CARDRE)
+      let preCard = descriptionString.slice(0, index)
+      descriptionString = descriptionString.slice(index)
+      descriptionString = descriptionString.replace("[[" + cardName + "]]", "")
+      return (
+        <>
+          {preCard}
+          <CardLink name={cardName} />
+        </>
+      )
+    })
+    description.push(<>{descriptionString}</>)
+  }
 
-  useEffect(() => {},
-  [])
-
-
-
-  return (<>{description}</>)
+  return <>{description}</>
 }
 
 export default CardDescription
--- src/components/tablerow.js
+++ src/components/tablerow.js
@@ -2,26 +2,31 @@ import React, { useState, useRef, useEffect } from "react"
 import Table from "react-bootstrap/Table"
 import Button from "react-bootstrap/Button"
 import { FaClipboardCheck } from "react-icons/fa"
-import Overlay from 'react-bootstrap/Overlay'
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
-import Tooltip from 'react-bootstrap/Tooltip'
-import Popover from 'react-bootstrap/Popover'
-import CardLink from './carddescription'
+import Overlay from "react-bootstrap/Overlay"
+import OverlayTrigger from "react-bootstrap/OverlayTrigger"
+import Tooltip from "react-bootstrap/Tooltip"
+import Popover from "react-bootstrap/Popover"
+import CardLink from "./carddescription"
 import CardDescription from "./carddescription"
 
 const TableRow = ({ data }) => {
-  const [copied, setCopied] = useState('copy')
+  const [copied, setCopied] = useState("copy")
   const [show, setShow] = useState(false)
   const target = useRef(null)
 
   function CopyToClipboard() {
     setShow(true)
-    setTimeout(()=>{setShow(false)},1000)
-    navigator.clipboard.writeText(data.Code).then(() => {
-      setCopied('Copied to Clipboard')
-    }).catch(()=>{
-      setCopied('There has been an error')
-    })
+    setTimeout(() => {
+      setShow(false)
+    }, 1000)
+    navigator.clipboard
+      .writeText(data.Code)
+      .then(() => {
+        setCopied("Copied to Clipboard")
+      })
+      .catch(() => {
+        setCopied("There has been an error")
+      })
   }
 
   function renderTooltip(props) {
@@ -31,24 +36,34 @@ const TableRow = ({ data }) => {
       </Tooltip>
     )
   }
-  
+
   function renderNothing() {
     return <div></div>
   }
-
-  
 
   return (
     <tr>
       <td>
         {data.Code}
-        <OverlayTrigger placement="right" trigger="click" overlay={show ? renderTooltip : renderNothing}>
-          <Button ref={target} style={{float: 'right'}}size="sm" variant="outline-primary" onClick={CopyToClipboard}>
+        <OverlayTrigger
+          placement="right"
+          trigger="click"
+          overlay={show ? renderTooltip : renderNothing}
+        >
+          <Button
+            ref={target}
+            style={{ float: "right" }}
+            size="sm"
+            variant="outline-primary"
+            onClick={CopyToClipboard}
+          >
             <FaClipboardCheck />
-          </Button>     
+          </Button>
         </OverlayTrigger>
       </td>
-      <td><CardDescription description={data.Description} /></td>
+      <td>
+        <CardDescription description={data.Description} />
+      </td>
       <td>{data.Expiry}</td>
       <td>{data.Date_Added}</td>
     </tr>
