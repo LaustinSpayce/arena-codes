07b23ae0724e66b6e223ebfe6e05384e4a75f169
Stuart Myers <stuart.myers@gmail.com>
2020-04-15 15:41:43 +0800

    Added scryfall search for card names

---
+++ src/components/cardlink.js
@@ -0,0 +1,7 @@
+import React, { useState, useRef, useEffect } from "react"
+
+const CardLink = (data) => {
+  return (<div>aaa</div>)
+}
+
+export default CardLink
--- src/components/tablerow.js
+++ src/components/tablerow.js
@@ -1,11 +1,71 @@
-import React from "react"
+import React, { useState, useRef, useEffect } from "react"
 import Table from "react-bootstrap/Table"
+import Button from "react-bootstrap/Button"
+import { FaClipboardCheck } from "react-icons/fa"
+import Overlay from 'react-bootstrap/Overlay'
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
+import Tooltip from 'react-bootstrap/Tooltip'
+import Popover from 'react-bootstrap/Popover'
+import CardLink from './cardlink'
+
+const CARDRE = /\[\[([^\]]+)\]\]/g;
+const SCRYFALL = 'https://scryfall.com/search?q='
 
 const TableRow = ({ data }) => {
+  const [copied, setCopied] = useState('copy')
+  const [show, setShow] = useState(false)
+  const [description, setDescription] = useState(data.Description)
+  const target = useRef(null)
+
+  useEffect(() => {
+    let cardDetect = data.Description.match(CARDRE)
+    if (cardDetect) {
+      let tempString = cardDetect[0]
+      console.log('temp string', tempString)
+      tempString = tempString.replace('[[', '')
+      tempString = tempString.replace(']]', '')
+      let searchURL = SCRYFALL + tempString
+      setDescription(<div><a href={searchURL}>{tempString}</a> card style</div>)
+    } else {
+      setDescription(data.Description)
+    }
+  }, [])
+
+  function CopyToClipboard() {
+    setShow(true)
+    setTimeout(()=>{setShow(false)},1000)
+    navigator.clipboard.writeText(data.Code).then(() => {
+      setCopied('Copied to Clipboard')
+    }).catch(()=>{
+      setCopied('There has been an error')
+    })
+  }
+
+  function renderTooltip(props) {
+    return (
+      <Tooltip id="button-tooltip" {...props}>
+        Copied to Clipboard
+      </Tooltip>
+    )
+  }
+  
+  function renderNothing() {
+    return <div></div>
+  }
+
+  
+
   return (
     <tr>
-      <td>{data.Code}</td>
-      <td>{data.Description}</td>
+      <td>
+        {data.Code}
+        <OverlayTrigger placement="right" trigger="click" overlay={show ? renderTooltip : renderNothing}>
+          <Button ref={target} style={{float: 'right'}}size="sm" variant="outline-primary" onClick={CopyToClipboard}>
+            <FaClipboardCheck />
+          </Button>     
+        </OverlayTrigger>
+      </td>
+      <td>{description}</td>
       <td>{data.Expiry}</td>
       <td>{data.Date_Added}</td>
     </tr>
