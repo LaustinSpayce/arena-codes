3a2912030d7ea1f3a28177f0e7fd63856ae6c4a8
Stuart Myers <stuart.myers@gmail.com>
2020-04-15 19:20:11 +0800

    Adjust

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,15 +7,15 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    // {
-    //   resolve: `gatsby-source-filesystem`,
-    //   options: {
-    //     name: `images`,
-    //     path: `${__dirname}/src/images`,
-    //   },
-    // },
-    // `gatsby-transformer-sharp`,
-    // `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -27,8 +27,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `mtg-arena-codes`,
+        short_name: `arena-codes`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
