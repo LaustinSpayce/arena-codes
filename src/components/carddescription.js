b0b5989f2cd742ebfaf54843d6a3287f9af542c7
Stuart Myers <stuart.myers@gmail.com>
2020-12-02 01:39:59 +0800

    final version to public

--- src/components/carddescription.js
+++ src/components/carddescription.js
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from "react"
 import OverlayTrigger from "react-bootstrap/OverlayTrigger"
 import Popover from "react-bootstrap/Popover"
 
-const SCRYFALL_SEARCH = "https://scryfall.com/search?q="
+const SCRYFALL_SEARCH = "https://www.scryfall.com/search?q="
+const SCRYFALL_GET_BY_SET = "https://api.scryfall.com/cards/"
+const SCRYFALL_LINK_DIRECT = "https://scryfall.com/card/"
 const SCRYFALL_IMAGE =
   "https://api.scryfall.com/cards/named?format=image&version=medium&exact="
 const CARDRE = /\[\[([^\]]+)\]\]/
@@ -10,17 +12,30 @@ const CARDRE = /\[\[([^\]]+)\]\]/
 const CardLink = (card) => {
   const [cardName, setCardName] = useState(card.name)
   const [setName, setSetName] = useState("")
+  const [cardNo, setCardNo] = useState("")
   const placement = card.placement ? card.placement : 'top'
   useEffect(() => {
     if (cardName.includes("|")) {
       let substrings = cardName.split("|")
       setCardName(substrings[0])
-      setSetName("&set=" + substrings[1])
+      setSetName(substrings[1].toLowerCase())
+      console.log(substrings.length)
+      if (substrings.length >= 3) {
+        setSetName(substrings[1].toLowerCase())
+        setCardNo(substrings[2])
+      }
     }
   }, [cardName])
 
-  const fullURL = SCRYFALL_SEARCH + cardName + setName
-  const imageURL = SCRYFALL_IMAGE + cardName + setName
+  const fullURL = cardNo ? 
+                    `${SCRYFALL_LINK_DIRECT}${setName}/${cardNo}` :
+                    `${SCRYFALL_SEARCH}${cardName}` 
+
+  const imageURL = cardNo ? 
+                    `${SCRYFALL_GET_BY_SET}${setName}/${cardNo}/?format=image` : 
+                    setName ?
+                      `${SCRYFALL_IMAGE}${cardName}${setName}` :
+                      `${SCRYFALL_IMAGE}${cardName}`
 
   const popover = (
     <Popover id="popover-basic" style={{ maxWidth: "100%" }}>
